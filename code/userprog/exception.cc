6dbb9fb82c198b1dc620e322cbb73ef498855647
Thomas Loubiou <xabufr@gmail.com>
2016-10-04 10:17:58 +0200

    Indentation

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -108,13 +108,13 @@ ExceptionHandler (ExceptionType which)
 	      synchConsole->SynchPutString(stringBuffer);
 	      break;
 	    }
-          case SC_GetChar:
-            {
-              DEBUG('s', "GetChar\n");
-              int character = synchConsole->SynchGetChar();
-              machine->WriteRegister(2, character);
-              break;
-            }
+      case SC_GetChar:
+        {
+            DEBUG('s', "GetChar\n");
+            int character = synchConsole->SynchGetChar();
+            machine->WriteRegister(2, character);
+            break;
+        }
 #endif //CHANGED
 	  default:
 	    {
